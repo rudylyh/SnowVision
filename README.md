1104b11f70b223786e79f7f177f52cdc80e208a7
Yuhang Lu <rudylyh@gmail.com>
2020-06-17 00:36:10 -0400

    test

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 ## Introduction
 [SnowVision](https://github.com/rudylyh/SnowVision) is a project of exploring the application of deep learning and computer vision techniques in traditional archeological heritage fragment recognition. It is part of the World Engraved project ([http://worldengraved.org](http://worldengraved.org/index)).
 
-The aim of SnowVision is to automate the matching process between fragmentary stamped pottery sherds from the archaeological record and their appropriate complete paddle design from among the hundreds of registered design reconstructions in the corpus. Example of sherds and designs are shown below.
+The aim of SnowVision is to automate the matching process between fragmentary stamped pottery sherds from the archaeological record and their appropriate complete paddle design from among the hundreds of registered design reconstructions in the corpus. Example of sherds and designs are shown as below.
 
 <p align="center">
 <img src="data/eg_img.png" width="80%"/>
+</p>
 
 Technically, our goal is to find the ground truth design of the query sherd in the design database and the corresponding location.
 
 <p align="center">
 <img src="data/match_goal.png"/>
 </p>
+
 
 ## How to use
 #### Requirements:
@@ -20,9 +22,44 @@ Technically, our goal is to find the ground truth design of the query sherd in t
 - skimage==0.10.1
 
 #### Input:
+- point cloud of query sherd in the format of xyz file (./data/split_xyz)
+- design database (./data/designs)
+
 #### Output:
+- depth image of query sherd (./data/depth)
+- mask of query sherd (./data/mask)
+- extracted curve of query sherd (./data/mask)
+- matching result of query sherd (./data/match_result/xxx/final.xlsx)
+
+Example of matching result table:
+<p align="center">
+<img src="data/match_table.png" width="90%"/>
+</p>
+
 #### Before running:
+- Use makefile to build a xyz processing library named 'libxyz_proc.so'
+
+    `$ make`
+
 #### Usage example:
+- For one sherd:
+
+    `$ python main.py --xyz-dir ./data/split_xyz --xyz-name SCAN000702.xyz`
+
+- For batch processing (all xyz files in the directory):
+
+    `$ python main.py --xyz-dir ./data/split_xyz`
+
+- The result paths could be specified with parameters of '--depth-dir', '--curve-dir', '--mask-dir' and '--res-dir'.
+
+
+#### Experimental result:
+We use our method to match 247 raw sherds with a database of 100 designs, and get the below CMC curve. It means Y% of sherds could find the correct design in top-X matches. Each sherd takes about 5-10 mins.
+<p align="center">
+<img src="data/cmc_curve.png" width="90%"/>
+</p>
+
+
 
 ## Citation
 ```
---
+++ curve_extraction.py
@@ -0,0 +1,106 @@
+import cv2
+import numpy as np
+from skimage.morphology import skeletonize
+
+
+def Normalize(image):
+    min_val = min(map(min, image))
+    image = image - min_val
+    max_val = max(map(max, image))
+    image = image / max_val
+    image = image * 255
+    return image.astype(np.uint8)
+
+
+def ImagePadding(img, k):
+    padded_img = np.zeros((img.shape[0] + 2*k, img.shape[1] + 2*k), np.uint8)
+    padded_img[k:k+img.shape[0],k:k+img.shape[1]] = img
+    return padded_img
+
+
+def Softmax(x):
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
+def RemoveSmallArea(img, thre):
+    # img2, contours, hierarchy = cv2.findContours(img.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    contours, hierarchy = cv2.findContours(img.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area < thre:
+            cv2.fillPoly(img, pts =[contour], color=(0))
+
+
+#depth image to skeleton heatmap
+def depth2hm(depth_img, cen_net):
+    cen_input = depth_img[np.newaxis, ...]
+    cen_net.blobs['data'].reshape(1, *cen_input.shape)
+    cen_net.blobs['data'].data[...] = cen_input
+    cen_net.forward()
+    hm_img = cen_net.blobs['output'].data[0][1,:,:]
+    hm_img = Normalize(hm_img) #heatmap
+    return hm_img
+
+
+#skeleton heatmap to binary skeleton image
+def hm2skel(hm_img):
+    raw_curve_img = np.zeros((hm_img.shape), np.uint8)
+    raw_curve_img[hm_img<=128] = 0;
+    raw_curve_img[hm_img>128] = 1;
+    skel_bool_img = skeletonize(raw_curve_img)
+    raw_skel_img = np.zeros((raw_curve_img.shape), np.uint8)
+    raw_skel_img[skel_bool_img == True] = 255
+    return raw_skel_img
+
+
+#refine raw skeleton image, the size of cropped image patch is 2k+1
+def RefineSkel(hm_img, raw_skel_img, pcn_net, k = 25):
+    pad_hm_img = ImagePadding(hm_img, k)
+    pad_raw_skel_img = ImagePadding(raw_skel_img, k)
+    skel_img = np.zeros(raw_skel_img.shape, np.uint8)
+    for r in range(k, pad_raw_skel_img.shape[0]-k):
+        for c in range(k, pad_raw_skel_img.shape[1]-k):
+            if pad_raw_skel_img[r,c] == 255:
+                hm_patch = pad_hm_img[r-k:r+k+1, c-k:c+k+1]
+                pcn_net.blobs['data'].data[...] = hm_patch
+                pcn_net.forward()
+                prob = Softmax(pcn_net.blobs['fc5'].data[0])[1]
+                if prob > 0.5:
+                    skel_img[r-k, c-k] = 255
+    return skel_img
+
+
+#recover skeleton image to curve image
+def skel2curve(depth_img, mask_img, skel_img):
+    ori_curve_img = np.zeros(depth_img.shape, np.uint8)
+    p = 9 #search radius
+    for r in range(skel_img.shape[0]):
+        for c in range(skel_img.shape[1]):
+            if skel_img[r,c]==255:
+                (min_val, max_val) = (255, 0)
+                pixel_list = []
+                for x in range(max(0, r-p), min(skel_img.shape[0], r+p+1)):
+                    for y in range(max(0, c-p), min(skel_img.shape[1], c+p+1)):
+                        if (r-x)*(r-x)+(c-y)*(c-y) < p*p:
+                            pixel_list.append((x,y))
+                            if depth_img[x,y] > max_val:
+                                max_val = depth_img[x,y]
+                            if depth_img[x,y] < min_val:
+                                min_val = depth_img[x,y]
+                thre = min_val + (depth_img[r,c]-min_val)/2.0
+                for pixel in pixel_list:
+                    if depth_img[pixel] > thre:
+                        ori_curve_img[pixel] = 255
+    curve_img = cv2.GaussianBlur(ori_curve_img, (27,27), 0);
+    ret, curve_img = cv2.threshold(curve_img, 127, 255, cv2.THRESH_BINARY)
+    RemoveSmallArea(curve_img, 500)
+    curve_img[mask_img == 0] = 0
+    return curve_img
+
+
+def depth2curve(depth_img, mask_img, cen_net, pcn_net):
+    hm_img = depth2hm(depth_img, cen_net)
+    raw_skel_img = hm2skel(hm_img)
+    skel_img = RefineSkel(hm_img, raw_skel_img, pcn_net)
+    curve_img = skel2curve(depth_img, mask_img, skel_img)
+    return curve_img
