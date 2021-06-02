94ded04d4047d68ef2a3f3da8bad910b5b6fc3d9
Yuhang Lu <rudylyh@gmail.com>
2021-06-02 15:19:13 -0400

    add hrnet

--- seperate_functions.py
+++ seperate_functions.py
@@ -19,7 +19,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--config-file', type=str, default='./config.json', help='json of parameter settings')
 parser.add_argument('--input', type=str, help='a zip file of raw scans or a directory')
 parser.add_argument('--scan-name', type=str, default=None, help='process the whole input folder if None')
-parser.add_argument('--design-dir', type=str, default='./designs', help='a folder of design images')
+parser.add_argument('--design-dir', type=str, default='/WD1/SnowVision/data/designs', help='a folder of design images')
 parser.add_argument('--output', type=str, help='a zip file of matching results or a directory')
 args = parser.parse_args()
 
@@ -30,7 +30,7 @@ xyz_proc_lib.SplitCloud.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_c
 
 
 ''' Load CNN models '''
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # device = torch.device('cuda:1')
 cen_net = seg_hrnet.get_seg_model('./model/hrnet_config.yaml', num_classes=2)
 main_gpu = torch.device('cuda:1')
@@ -40,32 +40,15 @@ cen_net.module.load_state_dict(checkpoint['state_dict'])
 cmn_net = CMN()
 cmn_ckp = torch.load('./model/cmn.pth', map_location=lambda storage, loc: storage)
 cmn_net.load_state_dict(cmn_ckp)
-cmn_net.to(device)
+cmn_net.to(main_gpu)
 matcher = Matcher(opts, cmn_net, args.design_dir)
 
 
-if __name__ == "__main__":
-    if args.input.endswith('zip'):
-        # Create two temporary folders to store inputs and outputs
-        in_dir = './tmp_input'
-        out_dir = './tmp_output'
-        os.mkdir(in_dir)
-        os.mkdir(out_dir)
-        # Unzip all files and process one by one
-        zip_reader = ZipFile(args.input, 'r')
-        zip_reader.extractall(in_dir)
-        zip_reader.close()
-    else:
-        in_dir = args.input
-        out_dir = args.output
-
-    if args.scan_name is None:
-        name_list = os.listdir(args.in_dir)
-    else:
-        name_list = [args.scan_name]
-
-    for i, scan_name in enumerate(name_list):
-        print("Splitting scan:", scan_name, f'({i}/{len(name_list)})')
+def get_depth():
+    in_dir = '/WD1/SnowVision/data/usf_xyx_batch1/original_xyz'
+    out_dir = '/WD1/SnowVision/data/usf_xyx_batch1/output'
+    for i, scan_name in enumerate(os.listdir(in_dir)):
+        print(i, scan_name)
         split_num = xyz_proc_lib.SplitCloud(
                         bytes(in_dir, encoding='utf8'),
                         bytes(scan_name, encoding='utf8'),
@@ -78,7 +61,7 @@ if __name__ == "__main__":
             print(split_num, "sherd(s) found.")
 
         for i in range(0, split_num):
-            sherd_name = scan_name.split('.')[0] + '-' + str(i+1)
+            sherd_name = os.path.splitext(scan_name)[0] + '-' + str(i+1)
             print("--- Processing sherd:", sherd_name)
             # try:
             pt_cloud = ReadXYZ(os.path.join(out_dir, sherd_name, 'sherd.xyz'))
@@ -88,17 +71,30 @@ if __name__ == "__main__":
             cv2.imwrite(os.path.join(out_dir, sherd_name, 'depth.png'), depth_img)
             cv2.imwrite(os.path.join(out_dir, sherd_name, 'mask.png'), mask_img)
 
-            print("------ Extracting curve image")
-            curve_img = depth2curve(depth_img, mask_img, cen_net)
-            cv2.imwrite(os.path.join(out_dir, sherd_name, 'curve.png'), curve_img)
 
-            print("------ Matching with all designs")
-            top_k_match = matcher.GetTopKMatch(sherd_name.split('.')[0], depth_img, curve_img, mask_img, args.design_dir)
-            matcher.WriteMatchResults(top_k_match, os.path.join(out_dir, sherd_name))
-            # except:
-            #     print("------ Failed.")
+def match_curve():
+    data_dir = '/home/rudy/WD1/SnowVision/exp_lsc_scan'
+    save_dir = '/WD1/SnowVision/data/Karen20210111_top100/top100_match_result'
+    sherd_list = os.listdir('/WD1/SnowVision/data/Karen20210111_top100/top100_sherds')
+    for i, sherd_name in enumerate(sherd_list):
+        sherd_name = sherd_name.split('.')[0]
+        print(i, sherd_name)
+        depth_img = cv2.imread(os.path.join(data_dir, 'depth', sherd_name+'.png'), 0)
+        mask_img = cv2.imread(os.path.join(data_dir, 'mask', sherd_name+'.png'), 0)
+        # curve_img = cv2.imread(os.path.join(src_dir, sherd_name, 'curve.png'), 0)
+        curve_img = depth2curve(depth_img, mask_img, cen_net)
+        # cv2.imshow('depth_img', depth_img)
+        # cv2.imshow('curve_img', curve_img)
+        # cv2.waitKey()
+        top_k_match = matcher.GetTopKMatch(sherd_name, depth_img, curve_img, mask_img, args.design_dir)
+        tmp_save_dir = os.path.join(save_dir, sherd_name)
+        if not os.path.exists(tmp_save_dir):
+            os.mkdir(tmp_save_dir)
+        shutil.copyfile(os.path.join(data_dir, 'depth', sherd_name+'.png'), os.path.join(tmp_save_dir, 'depth.png'))
+        shutil.copyfile(os.path.join(data_dir, 'mask', sherd_name+'.png'), os.path.join(tmp_save_dir, 'mask.png'))
+        cv2.imwrite(os.path.join(tmp_save_dir, 'curve.png'), curve_img)
+        matcher.WriteMatchResults(top_k_match, tmp_save_dir)
 
-    if args.output.endswith('zip'):
-        shutil.make_archive(args.output[:-4], 'zip', out_dir)
-        shutil.rmtree(in_dir)
-        shutil.rmtree(out_dir)
+
+if __name__ == "__main__":
+    get_depth()
